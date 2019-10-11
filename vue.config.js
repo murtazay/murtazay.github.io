7855ced684d1e7a6216551f89dd72d0136a268bd
Murtaza Yaqoob <murtazay@live.com>
2019-10-11 19:08:35 -0400

    Added Pages
    Started with index page

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  runtimeCompiler: true
+    transpileDependencies: ["vuetify"],
+    runtimeCompiler: true,
+    pages:{
+        index:{
+            entry:'src/pages/index/main.js',
+            template:'public/index.html',
+            filename:'index.html',
+            title:'MurtazaY@Github',
+        }
+    }
 };
